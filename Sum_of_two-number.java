eb742c7e26aa764e77378fa09cbaddfbde16746f
Patel Rishikumar <153611518+rishiprp@users.noreply.github.com>
2025-07-10 17:51:59 +0530

    Create Sum of two number.java

---
+++ Sum_of_two-number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);//creating object of Scanner class 
+        System.out.print("Enter the Value of A :");
+        int a = sc.nextInt();//take input as integer 
+        System.out.print("Enter the VAlue of B :");
+        int b = sc.nextInt();//take input as integer 
+        int sum= a+b;
+        System.out.println("Sum of Two number is " + sum);
+    }
+}
